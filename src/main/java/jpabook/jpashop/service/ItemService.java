709edd51ca8cefd9f9bcea9673216797a9a9bd1c
Tamhee_Lee <tamhee0110@naver.com>
2023-02-20 23:43:36 +0900

    회원가입, 회원목록, 상품등록, 상품목록, 상품주문, 주문내역 조회 및 수정

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -27,4 +27,12 @@ public class ItemService {
     public Item findOne(Long itemId){
         return itemRepository.findOne(itemId);
     }
+
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+    }
 }
